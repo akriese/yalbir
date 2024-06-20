253cc098b0c604ce727fcaff00227779b67d37cf
Anton Kriese <anton.kriese@gmx.de>
2024-06-20 13:36:19 +0200

    feat(shooting_star): coerce star speeds

--- src/patterns/shooting_star.rs
+++ src/patterns/shooting_star.rs
@@ -4,7 +4,7 @@ const MAX_STARS: usize = 10;
 
 pub struct ShootingStar {
     rgbs_current: [Rgb; N_LEDS],
-    speed_bounds: (u8, u8),
+    speed_bounds: (usize, usize),
     stars: [Star; MAX_STARS],
 }
 
@@ -17,7 +17,7 @@ struct Star {
 }
 
 impl ShootingStar {
-    pub fn new(speed_bounds: (u8, u8)) -> Self {
+    pub fn new(speed_bounds: (usize, usize)) -> Self {
         ShootingStar {
             rgbs_current: [Rgb::default(); N_LEDS],
             speed_bounds,
@@ -31,10 +31,19 @@ impl ShootingStar {
         }
 
         for s in self.stars.iter_mut() {
+            if s.speed == 0 {
+                continue;
+            }
+
             s.position += s.speed as usize;
 
+            // deactivate star if it is out of bounds (including the tail)
+            if s.position as i32 - s.tail_length as i32 >= N_LEDS as i32 {
+                s.speed = 0;
+            }
+
             for i in 0..s.tail_length {
-                let pos = (s.position as i32 - i as i32);
+                let pos = s.position as i32 - i as i32;
                 if pos < N_LEDS as i32 && pos >= 0 {
                     self.rgbs_current[pos as usize]
                         .add(&s.color.scaled(100 - (100 * i / s.tail_length) as u8));
@@ -61,8 +70,18 @@ impl ShootingStar {
         self.stars[index] = Star {
             color,
             position: 0,
-            speed,
+            speed: self.coerce_speed(speed),
             tail_length,
         };
+    }
+
+    fn coerce_speed(&self, speed: usize) -> usize {
+        if speed > self.speed_bounds.1 {
+            self.speed_bounds.1
+        } else if speed < self.speed_bounds.0 {
+            self.speed_bounds.0
+        } else {
+            speed
+        }
     }
 }
