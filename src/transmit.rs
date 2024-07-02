acef7debf551d8d854d81663a2835c59e683ab42
Anton Kriese <anton.kriese@gmail.com>
2024-07-02 14:21:06 +0200

    fix: use static mut array for RMT PulseCodes
    this avoids the panic when switching between wifi and render tasks. The
    RMT send takes a reference (to now a static array instead of a local)
    and that didnt go well when interrupted by the wifi timer, leading to
    stack corruption (presumably). Using the static mut array seems to solve
    the problem. This adds some unsafe code, but as only one thread ever
    works with the array, this should generally be save

--- src/transmit.rs
+++ src/transmit.rs
@@ -2,27 +2,36 @@ use esp_hal::{
     clock::Clocks,
     peripheral::Peripheral,
     peripherals::RMT,
-    rmt::{Channel, PulseCode, Rmt, TxChannel, TxChannelConfig, TxChannelCreator},
+    rmt::{
+        Channel, PulseCode, Rmt, SingleShotTxTransaction, TxChannel, TxChannelConfig,
+        TxChannelCreator,
+    },
     Blocking,
 };
 use fugit::HertzU32;
 
 use crate::{util::color::Rgb, N_LEDS};
 
-const NS_PER_CLOCK_CYCLE: u16 = (1000_000_000_f32 / 80_000_000_f32) as u16;
+const NS_PER_CLOCK_CYCLE: u16 = (1_000_000_000_f32 / 80_000_000_f32) as u16;
 const T0_HIGH: u16 = 350 / NS_PER_CLOCK_CYCLE;
 const T1_HIGH: u16 = 700 / NS_PER_CLOCK_CYCLE;
 const T_LOW_GAP: u16 = 600 / NS_PER_CLOCK_CYCLE;
 const T_LOW_RESET: u16 = 6000 / NS_PER_CLOCK_CYCLE;
+
+static mut RMT_ENCODING: [PulseCode; N_LEDS * 24] = [PulseCode {
+    level1: true,
+    length1: 0,
+    level2: true,
+    length2: 0,
+}; N_LEDS * 24];
 
 pub fn init_rmt<'d, P: esp_hal::gpio::OutputPin>(
     rmt: impl Peripheral<P = RMT> + 'd,
     pin: impl Peripheral<P = P> + 'd,
     clocks: &Clocks,
 ) -> Channel<Blocking, 0> {
-    let _rmt = Rmt::new(rmt, HertzU32::MHz(80), &clocks, None).unwrap();
-    let channel = _rmt
-        .channel0
+    let _rmt = Rmt::new(rmt, HertzU32::MHz(80), clocks, None).unwrap();
+    _rmt.channel0
         .configure(
             pin,
             TxChannelConfig {
@@ -35,15 +44,19 @@ pub fn init_rmt<'d, P: esp_hal::gpio::OutputPin>(
                 carrier_level: false,
             },
         )
-        .unwrap();
-
-    return channel;
+        .unwrap()
 }
 
 pub fn send_data(data: &[Rgb; N_LEDS], channel: Channel<Blocking, 0>) -> Channel<Blocking, 0> {
-    let mut send_data = [PulseCode::default(); N_LEDS * 24];
-    let mut ch = channel;
-    for (i, rgb) in data.iter().enumerate() {
+    let transaction = send_data_no_wait(data, channel);
+    transaction.wait().unwrap()
+}
+
+fn send_data_no_wait(
+    rgb_data: &[Rgb; N_LEDS],
+    channel: Channel<Blocking, 0>,
+) -> SingleShotTxTransaction<Channel<Blocking, 0>, PulseCode> {
+    for (i, rgb) in rgb_data.iter().enumerate() {
         // WS2818 LED strip expects the order green, then red, then blue
         // each component's most significant bit has to be sent first up until the least
         // significant bit. Timings inspired by
@@ -66,16 +79,13 @@ pub fn send_data(data: &[Rgb; N_LEDS], channel: Channel<Blocking, 0>) -> Channel
                         length2: T_LOW_GAP,
                     }
                 };
-                send_data[i * 24 + j * 8 + 7 - bit_pos] = code;
+                unsafe { RMT_ENCODING[i * 24 + j * 8 + 7 - bit_pos] = code };
             }
         }
     }
 
     // tell the RMT to stop transmitting with a zero-length second pulse
-    send_data.last_mut().unwrap().length2 = 0;
+    unsafe { RMT_ENCODING.last_mut().unwrap().length2 = 0 };
 
-    let transaction = ch.transmit(&send_data);
-    ch = transaction.wait().unwrap();
-
-    ch
+    channel.transmit(unsafe { &RMT_ENCODING })
 }
