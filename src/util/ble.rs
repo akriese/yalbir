3c72e6e2e6d25bf8eb3c8d9df20cfc339b0bacfc
Anton Kriese <anton.kriese@gmail.com>
2024-08-04 21:20:43 +0200

    feat: send whole reply via BLE
    before, the notification would only send the first 20 chars out, as the
    default MTU (maximum transmission units) is 23 per default.
    3 bytes are needed for packet meta data

--- src/util/ble.rs
+++ src/util/ble.rs
@@ -6,14 +6,17 @@ use bleps::async_attribute_server::AttributeServer;
 use bleps::asynch::Ble;
 use bleps::attribute_server::NotificationData;
 use bleps::gatt;
-use core::cell::RefCell;
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_sync::signal::Signal;
+use embassy_time::Timer;
 use esp_wifi::ble::controller::asynch::BleConnector;
 
 use crate::util::commands::handle_wireless_input;
 
-static COMMAND_REPLY: Signal<CriticalSectionRawMutex, String> = Signal::new();
+const INFO_PACKET_LENGTH: usize = (bleps::attribute_server::MTU - 3) as usize;
+
+static COMMAND_REPLY: Signal<CriticalSectionRawMutex, ()> = Signal::new();
+static mut REPLY: String = String::new();
 
 #[embassy_executor::task]
 pub(crate) async fn ble_handling(mut ble: Ble<BleConnector<'static>>) {
@@ -40,7 +43,8 @@ pub(crate) async fn ble_handling(mut ble: Ble<BleConnector<'static>>) {
             log::info!("RECEIVED: Offset {}, data {:?}", offset, data);
             let res = handle_wireless_input(core::str::from_utf8(data).unwrap());
             if let Err(err) = res {
-                COMMAND_REPLY.signal(err.to_string())
+                unsafe { REPLY = err.to_string() };
+                COMMAND_REPLY.signal(())
             }
         };
 
@@ -57,12 +61,28 @@ pub(crate) async fn ble_handling(mut ble: Ble<BleConnector<'static>>) {
         let mut rng = bleps::no_rng::NoRng;
         let mut srv = AttributeServer::new(&mut ble, &mut gatt_attributes, &mut rng);
 
-        let counter = RefCell::new(0u8);
-        let counter = &counter;
-
         let mut notifier = || async {
-            let text = COMMAND_REPLY.wait().await;
-            NotificationData::new(socket_handle, text.as_bytes())
+            if unsafe { REPLY.is_empty() } {
+                COMMAND_REPLY.wait().await;
+            }
+
+            // this is somehow needed to not cause weird behavior on the client side
+            // as the messages seem to be sent too quickly
+            Timer::after_millis(50).await;
+
+            unsafe {
+                let next_notification: String;
+                if REPLY.len() < INFO_PACKET_LENGTH {
+                    next_notification = REPLY.to_string();
+                    REPLY = "".to_string();
+                } else {
+                    let (head, tail) = REPLY.split_at(INFO_PACKET_LENGTH);
+                    next_notification = head.to_string();
+                    REPLY = tail.to_string();
+                }
+
+                NotificationData::new(socket_handle, next_notification.as_bytes())
+            }
         };
 
         match srv.run(&mut notifier).await {
